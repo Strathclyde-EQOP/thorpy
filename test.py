8f83b23fa5f23500a5f6e65fed8050fbb663e5d5
Stuart Bennett <stuart@tania.eng.cam.ac.uk>
2019-09-20 17:02:08 +0100

    test.py: load configuration defaults

--- test.py
+++ test.py
@@ -7,6 +7,21 @@ if __name__ == '__main__':
     print(stages)
     s = stages[0]
     
+    s.home_velocity = s._conf_home_vel
+    s.max_velocity = s._conf_def_max_vel
+    s.acceleration = s._conf_def_accn
+    s.home_offset_distance = s._conf_home_zero_offset
+    s.cw_hard_limit = s._conf_cw_hard_limit
+    s.ccw_hard_limit = s._conf_ccw_hard_limit
+    s.cw_soft_limit = s._conf_cw_soft_limit
+    s.ccw_soft_limit = s._conf_ccw_soft_limit
+    s.software_limit_mode = s._conf_soft_limit_mode
+    s.backlash_dist = s._conf_backlash_dist
+    if s._port._info_message['model_number'].decode('ascii')[:5] == 'SCC20': # BSC20x
+        s.rest_power = s._conf_rest_factor
+        s.move_power = s._conf_move_factor
+        s._port.send_message(MGMSG_MOT_SET_KCUBEKSTLOOPPARAMS(s._chan_ident, 1, 200000, 1000, 100, 100000000, 200, 0, 0, 0))    # as set by APT
+
     s.home(force = True)
     
     s.print_state()
